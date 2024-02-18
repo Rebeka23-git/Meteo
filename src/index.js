20b35bd152efc2834d240aaf35cfa50a15a45865
Rebeka23-git <158557757+Rebeka23-git@users.noreply.github.com>
2024-02-18 19:11:36 +0100

    changed from built in data to real life data in displayForecast function

--- src/index.js
+++ src/index.js
@@ -50,6 +50,13 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["sun", "Mon", "Tue", "Wed", "Thu", "Fre", "Sat"];
+
+  return days[date.getDay()];
+}
 function getForecast(city) {
   let apiKey = "400fcc7471aod0afddf69b6c3tfef4a0";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -57,24 +64,27 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42" />
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
     <div class="weather-forecast-temperature">
-      <span class="weather-forecast-temperature-max">18°</span>
-      <span class="weather-forecast-temperature-min">12°</span>
+      <span class="weather-forecast-temperature-max">${Math.round(
+        day.temperature.maximum
+      )}°</span>
+      <span class="weather-forecast-temperature-min">${Math.round(
+        day.temperature.minimum
+      )}°</span>
     </div>
   </div>
 `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
